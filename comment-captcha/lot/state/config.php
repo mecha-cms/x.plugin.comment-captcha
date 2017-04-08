1146d027af9a80474e5f5e3666355e24076ca3c8
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-04-08 21:14:57 +0700

    Update

--- comment-captcha/lot/state/config.php
+++ comment-captcha/lot/state/config.php
@@ -3,8 +3,10 @@
 return [
     'type' => 'math', // the captcha type
     'types' => [
-        'math' => [1, 10], // function argument(s) for `Captcha::math()` after the `$id` parameter
-        'text' => [false, '000'], // function argument(s) for `Captcha::text()` after the `$id` parameter
-        'toggle' => [] // function argument(s) for `Captcha::toggle()` after the `$id` parameter
+        // function argument(s) for `Captcha::{$any}()` after the `$id` parameter
+        'math' => [1, 10], 
+        'text' => [false, '000'],
+        'toggle' => [],
+        'token' => []
     ]
 ];
